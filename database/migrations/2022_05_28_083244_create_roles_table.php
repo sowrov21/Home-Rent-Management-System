865983f55fbf69877de48f10d758e5516f422a58
sowrov21 <sowrov.it@gmail.com>
2022-05-29 09:58:14 +0600

    role migration problem fixed

--- database/migrations/2022_05_28_083244_create_roles_table.php
+++ database/migrations/2022_05_28_083244_create_roles_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
-            $table->name();
+            $table->string('name');
             $table->timestamps();
         });
     }
